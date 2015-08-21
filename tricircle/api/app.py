e94e98ad4a205e73e5fddb31ef1ef4187ae0b1d5
joehuang <joehuang@huawei.com>
2015-08-21 09:29:34 +0800

    Add cascade service REST api framework
    Fisrt patch to implement the REST api framework based on pecan, with keystone authentication integrated.
    No resources implemented in the first patch
    
    Change-Id: I8bff9f934fbba183f0b0e7b540fa755965c229a0
    implements: blueprint: https://blueprints.launchpad.net/tricircle/+spec/implement-rest-api-framework

---
+++ tricircle/api/app.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2015 Huawei, Tech. Co,. Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from keystonemiddleware import auth_token
+from oslo_config import cfg
+from oslo_middleware import request_id
+import pecan
+
+from tricircle.common import exceptions as t_exc
+
+
+def setup_app(*args, **kwargs):
+    config = {
+        'server': {
+            'port': cfg.CONF.bind_port,
+            'host': cfg.CONF.bind_host
+        },
+        'app': {
+            'root': 'tricircle.api.controllers.root.RootController',
+            'modules': ['tricircle.api'],
+            'errors': {
+                400: '/error',
+                '__force_dict__': True
+            }
+        }
+    }
+    pecan_config = pecan.configuration.conf_from_dict(config)
+
+    # app_hooks = [], hook collection will be put here later
+
+    app = pecan.make_app(
+        pecan_config.app.root,
+        debug=False,
+        wrap_app=_wrap_app,
+        force_canonical=False,
+        hooks=[],
+        guess_content_type_from_ext=True
+    )
+
+    return app
+
+
+def _wrap_app(app):
+    app = request_id.RequestId(app)
+
+    if cfg.CONF.auth_strategy == 'noauth':
+        pass
+    elif cfg.CONF.auth_strategy == 'keystone':
+        app = auth_token.AuthProtocol(app, {})
+    else:
+        raise t_exc.InvalidConfigurationOption(
+            opt_name='auth_strategy', opt_value=cfg.CONF.auth_strategy)
+
+    return app
