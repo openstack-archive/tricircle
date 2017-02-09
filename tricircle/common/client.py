5bd718a373299168df011f9c072838737a4e6b52
zhiyuan_cai <luckyvega.g@gmail.com>
2017-02-09 14:47:16 +0800

    Update library used in client module
    1. What is the problem
    keystoneclient.session.Session and keystoneclient.auth.token_endpoint.Token
    have been deprecated in keystoneclient 2.1.0 release, but client module in
    Tricircle has not been updated to use the new library
    
    2. What is the solution to the problem
    Update library from keystoneclient to keystoneauth1
    
    3. What the features need to be implemented to the Tricircle
    No new features
    
    Change-Id: I6bff99fb4d16d410b19afdb74d2442243e38ed39
    Closes-Bug: #1655845

--- tricircle/common/client.py
+++ tricircle/common/client.py
@@ -20,9 +20,8 @@ import six
 from six.moves import xrange
 import uuid
 
-from keystoneclient.auth.identity import v3 as auth_identity
-from keystoneclient.auth import token_endpoint
-from keystoneclient import session
+from keystoneauth1.identity import v3 as auth_identity
+from keystoneauth1 import session
 from keystoneclient.v3 import client as keystone_client
 from oslo_config import cfg
 from oslo_log import log as logging
@@ -230,8 +229,8 @@ class Client(object):
         return self._get_keystone_session().get_project_id()
 
     def _get_endpoint_from_keystone(self, cxt):
-        auth = token_endpoint.Token(cfg.CONF.client.identity_url,
-                                    cxt.auth_token)
+        auth = auth_identity.Token(cfg.CONF.client.identity_url,
+                                   cxt.auth_token)
         sess = session.Session(auth=auth)
         cli = keystone_client.Client(session=sess)
 
